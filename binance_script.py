8b5e8669acc6fde05c8f23f9f444e395869c6caf
andeen171 <zeander21@outlook.com.br>
2021-08-24 14:03:08 -0300

    1.0

--- binance_script.py
+++ binance_script.py
@@ -1,71 +1,81 @@
 import math
-import yaml
 from math import floor
 from datetime import datetime
+from binance.client import Client
 from binance.exceptions import BinanceAPIException
-
-# loads api credentials
-with open('auth.yml') as yml:
-    auth = yaml.load(yml, Loader=yaml.FullLoader)
-    api_key = auth['binance_api']
-    api_secret = auth['binance_secret']
 
 
 class BinanceTradeBot:
 
-    def __init__(self, client, code, jason, preco, dados):
-        self.client = client
-        self.status = jason['status']
-        self.crypto = code
-        self.last_sell = jason['last_sell']
-        self.last_buy = jason['last_buy']
-        self.price = preco
-        self.data = dados
+    def __init__(self, key, secret, crypto, info):
+        self.client = Client(key, secret)
+        self.status = info['status']
+        self.crypto = crypto
+        self.last_sell = info['last_sell']
+        self.last_buy = info['last_buy']
+        self.terms = info['terms']
+        self.price = float(self.client.get_symbol_ticker(symbol=f"{crypto}BUSD")['price'])
+        # Data for MA lines
+        self.data = self.client.get_historical_klines(f"{crypto}BUSD", self.client.KLINE_INTERVAL_1HOUR,
+                                                      f"{info['terms'][1]} hours ago UTC")
 
+    # Write here the method you want the bot to follow and determine when buy or sell
     def get_status(self):
-        long_term = 0
-        short_term = 0
-        for x in range(len(self.data) - 1):
-            long_term += float(self.data[x][4])
-            if x >= 6:
-                short_term += float(self.data[x][4])
-        long = long_term / 12
-        short = short_term / 6
+        """
+        Uses especifics MA lines to determine golden cross(buy) and death cross(sell)
+        """
+        short_term = self.terms[0]
+        long_term = self.terms[1]
+        long_data = 0
+        short_data = 0
+        loop = len(self.data) - 1
+        for x in range(loop):
+            long_data += float(self.data[x][4])
+            if x >= ((short_term - loop) * -1):
+                short_data += float(self.data[x][4])
+        long = long_data / long_term
+        short = short_data / short_term
         if long > short and self.status != 'waiting':
             self.status = 'SELL'
+            return True
         elif short > long and self.status != 'holding':
             self.status = 'BUY'
-        return self.status
+            return True
+        return False
 
-    def get_quantity(self):
+    def create_order(self):
+        """
+        Build an order object with the especific parameters and return it
+        """
         step_size = self.client.get_symbol_info(f'{self.crypto}BUSD')['filters'][2]['stepSize']
         precision = int(round(-math.log(float(step_size), 10), 0))
-        if self.status == 'BUY':
-            value = self.last_sell['value']
-            self.status = 'holding'
-            return round(floor((value / self.price) * 1000000) / 1000000, precision)
-        elif self.status == 'SELL':
-            return round(float(self.client.get_asset_balance(asset=self.crypto)['free']), precision)
-
-    def place_order(self):
-        quantity = self.get_quantity()
-        try:
-            self.client.create_test_order(symbol=f'{self.crypto}BUSD', side=self.status,
-                                          type='MARKET', quantity=quantity)
-        except BinanceAPIException as error:
-            print(error.status_code)
-            print(error.message)
-            return f"Failed to place an '{self.status}' order of {quantity} {self.crypto}"
+        order = {'time': str(datetime.now())}
+        if self.status == "SELL":
+            order['type'] = "SELL"
+            order['quantity'] = round(float(self.client.get_asset_balance(asset=self.crypto)['free']), precision)
+            order['value'] = round(order['quantity'] * self.price, 2)
+            self.last_sell = order
+            self.status = 'waiting'
         else:
-            value = round(quantity * self.price, 2)
-            order = {'amount': quantity, 'value': value, 'time': str(datetime.now())}
-            if self.status == 'BUY':
-                before = self.last_sell['amount']
-                margin = round(((quantity - before) / before) * 100, 2)
-                self.last_buy = order
-            else:
-                before = self.last_buy['value']
-                margin = round(((value - before) / before) * 100, 2)
-                self.last_sell = order
-            return f"Sucessfully did a '{self.status}' trade of {quantity} {self.crypto}\n" \
-                   f"with aprox. {str(margin)}% profit"
+            order['type'] = "BUY"
+            order['value'] = self.last_sell['value']
+            order['quantity'] = round(floor((order['value'] / self.price) * 1000000) / 1000000, precision)
+            self.last_buy = order
+            self.status = 'holding'
+        return order
+
+    def trade(self, order):
+        """
+        Do a trade based on the order object
+        """
+        s = order['type']
+        q = order['quantity']
+        v = order['value']
+        try:
+            # Change to create_order if you want it to spend your currency
+            self.client.create_test_order(symbol=f'{self.crypto}BUSD', side=s, type='MARKET', quantity=q)
+        except BinanceAPIException as error:
+            return ["Failed to do a '{s}' order in {self.crypto}\n",
+                    f"Error code: {error.status_code}\n{error.message}"]
+        return [f"Sucessfully did a '{s}' in {self.crypto}",
+                f"Trade of {q} for ${v}"]
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import json
 import yaml
-import discord
 import asyncio
-from time import sleep
+import discord
+from logger import log, embed
 from binance_script import BinanceTradeBot
-from binance.client import Client
 
-
+data = "data.json"
 with open('auth.yml') as yml:
     auth = yaml.load(yml, Loader=yaml.FullLoader)
     api_key = auth['binance_api']
@@ -29,33 +28,24 @@ class MyClient(discord.Client):
         await self.wait_until_ready()
         channel = self.get_channel(877341571173986325)  # channel ID goes here
         while True:
-            with open('data.json', 'r') as file:
+            with open(data, 'r') as file:
                 config = json.load(file)
-                api = Client(api_key, api_secret)
                 for crypto, info in config.items():
-                    price = float(api.get_symbol_ticker(symbol=f"{crypto}BUSD")['price'])
-                    data = api.get_historical_klines(f"{crypto}BUSD", api.KLINE_INTERVAL_1HOUR,
-                                                     "13 hours ago UTC")
-                    bot = BinanceTradeBot(api, crypto, info, price, data)
-                    bot.get_status()
-                    print(bot.status)
-                    if bot.status == 'BUY':
-                        await channel.send(bot.place_order())
-                        info['status'] = 'holding'
-                        info['last_buy'] = bot.last_buy
-                    elif bot.status == 'SELL':
-                        await channel.send(bot.place_order())
-                        info['status'] = 'waiting'
-                        info['last_sell'] = bot.last_sell
-                    else:
-                        await channel.send(f'No trades done for {crypto}')
-            with open('data.json', 'w') as file:
+                    bot = BinanceTradeBot(api_key, api_secret, crypto, info)
+                    if bot.get_status():
+                        order = bot.create_order()
+                        message = bot.trade(order)
+                        log(message)
+                        await channel.send(embed=embed(message))
+                    info['status'] = bot.status
+                    info['last_buy'] = bot.last_buy
+                    info['last_sell'] = bot.last_sell
+            with open(data, 'w') as file:
                 save = json.dumps(config, indent=4)
                 file.write(save)
-            await channel.send('now sleeping for 15min...')
-            sleep(900)
-            await asyncio.sleep(60)
+            await asyncio.sleep(300)
 
 
-dc = MyClient()
-dc.run(discord_key)
+if __name__ == '__main__':
+    dc = MyClient()
+    dc.run(discord_key)
